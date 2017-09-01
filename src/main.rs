92ed94d9fd0ee3696cd4cbf3c34dac91ea5eb9d2
Ross Harrison <rtharrison86@gmail.com>
2017-09-01 00:36:33 -0400

    print queens

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,33 @@ use std::collections::HashSet;
 #[macro_use]
 extern crate lazy_static;
 
+const SIZE: i32 = 8;
+const QUEEN: &'static str = "\u{265B}";
+
 lazy_static! {
     static ref OPTIONS: HashSet<i32> = [0,1,2,3,4,5,6,7].iter().cloned().collect();
 }
 
-fn main() {
-    for curr_item in OPTIONS.iter() {
-        println!("Hello, world! {:?}", curr_item);
+fn print_board() {
+    let line = "+---+---+---+---+---+---+---+---+";
+  
+    for row_position in OPTIONS.iter() {
+        println!("{}", line);;
+        for i in 0..SIZE {
+            print!("| ");
+
+            if *row_position == i {
+                print!("{} ", QUEEN);
+            }
+            else {
+                print!("  ");
+            }
+        }
+        print!("|\n");
     }
+    println!("{}", line);;
+}
+
+fn main() {
+    print_board();
 }
